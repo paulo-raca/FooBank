7f2b986d9021536c504778616e72c4a03925c665
Paulo Costa <paulo.costa@geofusion.com.br>
2016-04-14 03:50:52 -0300

    Add missing resources

---
+++ foobank-api/src/main/resources/META-INF/populate_database.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customer (id, name) VALUES (1, 'Jane Woods');
+INSERT INTO Customer (id, name) VALUES (2, 'Michael Li');
+INSERT INTO Customer (id, name) VALUES (3, 'Heidi Hasselbach');
+INSERT INTO Customer (id, name) VALUES (4, 'Rahul Pour');
+ALTER SEQUENCE hibernate_sequence RESTART WITH 5;
